483aa79c978cf52b0bed0f58b4f39ad2e0fba1cd
Binamra Lamsal <binamralamsal@gmail.com>
2022-08-12 09:47:15 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const allProducts = require("./products.json");
+const detailedProducts = require("./detailed-products.json");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/api/products", (req, res) => {
+  res.json(allProducts);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = detailedProducts.find( d => d.id === id);
+  if (!product) res.status(404).json({ error: "Product not found!" });
+  res.json(product);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
+});
