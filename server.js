fa1a64d666e1d261e903acdd847dff6ff572f0c2
thapatechnical <vbthapa55@gmail.com>
2022-11-15 16:13:19 +0530

    new improvement for all products

--- server.js
+++ server.js
@@ -10,18 +10,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/api/products", (req, res) => {
-  const { id } = req.query;
-  if (id) {
-    const product = detailedProducts.find( d => d.id === id);
-    if (!product) res.status(404).json({ error: "Product not found!" });
-    return res.json(product);
-  }
   return res.json(allProducts);
 });
 
 app.get("/api/products/:id", (req, res) => {
   const { id } = req.params;
-  const product = detailedProducts.find( d => d.id === id);
+  const product = detailedProducts.find((d) => d.id === id);
   if (!product) res.status(404).json({ error: "Product not found!" });
   res.json(product);
 });
